25fb37c5b8b2b94cfef32725204bdfcf19d6b3dc
tama <tama@ttk1.net>
2020-03-15 06:12:16 +0900

    やっぱりtypescriptやめる

--- main.js
+++ main.js
@@ -1,16 +1,49 @@
 // ==UserScript==
 // @name         emacsify
-// @namespace    http://tampermonkey.net/
-// @version      1.0
+// @namespace    https://ttk1.net/
+// @version      0.0
 // @description  Add emacs key binds.
 // @author       tama@ttk1.net
-// @match        https://ttk1.github.io/emacsify/
+// @match        https://ttk1.github.io/emacsify/*
 // @grant        none
 // ==/UserScript==
-function main() {
-    var textarea = document.getElementById('textarea');
-    textarea.innerHTML = 'hello, world\npiyo\nhuga';
-    textarea.focus();
-    textarea.setSelectionRange(13, 14);
-}
-main();
+
+const backward = function (target) {
+  if (target instanceof HTMLTextAreaElement) {
+    const current = target.selectionStart;
+    target.focus();
+    target.setSelectionRange(current - 1, current - 1);
+  }
+};
+
+const forward = function (target) {
+  if (target instanceof HTMLTextAreaElement) {
+    const current = target.selectionStart;
+    target.focus();
+    target.setSelectionRange(current + 1, current + 1);
+  }
+};
+
+const onKeyDown = function (event) {
+  if (event instanceof KeyboardEvent &&
+    event.ctrlKey &&
+    event.target instanceof HTMLTextAreaElement) {
+    let key = String.fromCharCode(event.keyCode);
+    switch (key) {
+      case 'B':
+        backward(event.target);
+        event.preventDefault();
+        break;
+      case 'F':
+        forward(event.target);
+        event.preventDefault();
+        break;
+    }
+  }
+};
+
+(() => {
+  const textarea = document.getElementById('textarea');
+  textarea.innerHTML = 'hello, world\npiyo\nhuga';
+  textarea.addEventListener('keydown', onKeyDown, false);
+})();
