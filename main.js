0646858548c644d80e9785dc21af2fc7cd7054b4
tama <tama@ttk1.net>
2020-03-15 21:44:49 +0900

    down実装

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-const backward = function (target) {
+const backward = (target) => {
   if (target instanceof HTMLTextAreaElement || target instanceof HTMLInputElement) {
     const current = target.selectionStart;
     if (current > 0) {
@@ -19,7 +19,7 @@ const backward = function (target) {
   }
 };
 
-const forward = function (target) {
+const forward = (target) => {
   if (target instanceof HTMLTextAreaElement || target instanceof HTMLInputElement) {
     const current = target.selectionStart;
     if (target instanceof HTMLTextAreaElement) {
@@ -34,16 +34,40 @@ const forward = function (target) {
   }
 };
 
-const keybinds = {
-  'B': backward,
-  'F': forward
+function getCurrentLineCol(current, text) {
+  const line = text.substring(0, current).split('\n').length - 1;
+  const col = current - text.split('\n').slice(0, line).join('\n').length - 1;
+  return [line, col];
 }
 
-const onKeyDown = function (event) {
+const down = (target) => {
+  if (target instanceof HTMLTextAreaElement) {
+    const current = target.selectionStart;
+    const rest = target.value.substring(current).split('\n');
+    if (rest.length <= 1) {
+      target.setSelectionRange(target.value.length, target.value.length);
+    } else {
+      const col = getCurrentLineCol(current, target.value)[1];
+      target.setSelectionRange(
+        current + rest[0].length + Math.min(col, rest[1].length) + 1,
+        current + rest[0].length + Math.min(col, rest[1].length) + 1
+      );
+    }
+  }
+};
+
+const keybinds = {
+  'B': backward,
+  'F': forward,
+  'I': down
+}
+
+const onKeyDown = (event) => {
   const target = document.activeElement;
   if (target instanceof HTMLTextAreaElement || target instanceof HTMLInputElement) {
     if (event instanceof KeyboardEvent && event.ctrlKey) {
       const key = String.fromCharCode(event.keyCode);
+      console.log(key)
       const keybind = keybinds[key];
       if (keybind) {
         keybind(target);
